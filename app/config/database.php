e959a418452cb4aebca56e894671ca42ef2789a1
Prashant Shah <pshah.smartgoalz@gmail.com>
2014-10-01 16:43:02 +0530

    Add MySQL schema
    Signed-off-by: Prashant Shah <pshah.smartgoalz@gmail.com>

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'smartgoalz_demo',
+			'database'  => 'smartgoalz',
 			'username'  => 'root',
-			'password'  => 'q1w2e3',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
